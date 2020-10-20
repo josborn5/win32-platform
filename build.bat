94a0f90c278062dbfed2e9cd7eaada19af0cd41b
John Osborn <john.osborn@relativity.com>
2020-10-20 18:45:29 -0500

    Adding /EHsc compiler flag as a work around for the C4577 warning appearing if the std::vector library is included with thorugh #include <vector>.

--- build.bat
+++ build.bat
@@ -14,11 +14,12 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 IF NOT EXIST %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 pushd %OUTPUT_DIR%
 
-SET COMMON_COMPILER_FLAGS=-MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4100 -wd4201 -DFLAG_SLOW=1 -DFLAG_WIN32=1 -FC -Z7 -Fmwin32_platform.map
+SET COMMON_COMPILER_FLAGS=-MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4100 -wd4201 -DFLAG_SLOW=1 -DFLAG_WIN32=1 -FC -Z7 -Fmwin32_platform.map /EHsc
 SET COMMON_LINKER_FLAGS=-opt:ref user32.lib Gdi32.lib winmm.lib
 
 REM 32-bit build
 REM cl %COMMON_COMPILER_FLAGS% ..\code\win32_platform.cpp /link -subsystem:windows,5.1  %COMMON_LINKER_FLAGS%
+
 
 REM 64-bit build
 cl %COMMON_COMPILER_FLAGS% ..\%CODE_DIR%\game.cpp /link %COMMON_LINKER_FLAGS%
