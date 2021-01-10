1e7458abf2a93106d7f18d4e523a3a6ed46487c2
John Osborn <josborn5@gmail.com>
2021-01-09 22:40:35 -0600

    Removing unneccessary linking from the unit test build.

--- build.bat
+++ build.bat
@@ -19,10 +19,9 @@ mkdir %OUTPUT_DIR%
 pushd %OUTPUT_DIR%
 
 SET COMMON_COMPILER_FLAGS=-MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4100 -wd4201 -FC -Z7 -Fmwin32_platform.map /EHsc /O2
-SET COMMON_LINKER_FLAGS=-opt:ref user32.lib Gdi32.lib winmm.lib
 
 REM 64-bit build
-cl.exe %COMMON_COMPILER_FLAGS% ..\%CODE_DIR%\unit_tests.cpp /link %COMMON_LINKER_FLAGS%
+cl.exe %COMMON_COMPILER_FLAGS% ..\%CODE_DIR%\unit_tests.cpp
 
 REM using the '/c' flag to skip linking and create only the '.obj' file
 cl.exe %COMMON_COMPILER_FLAGS% /c ..\%CODE_DIR%\gentle_giant.cpp
@@ -30,6 +29,8 @@ cl.exe %COMMON_COMPILER_FLAGS% /c ..\%CODE_DIR%\gentle_giant.cpp
 REM use the 'lib.exe' tool to create a lib file from the bj files
 lib.exe -nologo gentle_giant.obj
 
+REM build the game.exe file using the gentle_giant.lib
+SET COMMON_LINKER_FLAGS=-opt:ref user32.lib Gdi32.lib winmm.lib
 cl.exe %COMMON_COMPILER_FLAGS% ..\%CODE_DIR%\game.cpp /link %COMMON_LINKER_FLAGS% gentle_giant.lib
 
 xcopy ..\%CODE_DIR%\teapot.obj .
